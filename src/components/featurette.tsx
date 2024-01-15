fbd74a5c0ff8be6c408c870d85ec667ae2e278f5
Mark Ashworth <mp.ashworth@gmail.com>
2024-01-15 21:57:46 +0200

    Step 04 - Include qualifications

--- src/components/featurette.tsx
+++ src/components/featurette.tsx
@@ -1,40 +1,60 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
+
 
 const Featurette: React.FC = () => {
+    const { allFile } = useStaticQuery(
+        graphql`
+          query FeatureImageData {
+            allFile(filter: {relativeDirectory: {eq: "featurette"}}) {
+                edges {
+                    node {
+                        publicURL
+                        childrenImageSharp {
+                            gatsbyImageData(quality: 100)
+                        }
+                    }
+                }
+            }
+          }
+        `)
+
+
+    const featureImage = allFile.edges[0]?.node?.childrenImageSharp[0]?.gatsbyImageData;
+    
+
     return (
         <>
         <div className="container marketing">
             <div className="row featurette">
                 <div className="col-md-12 order-md-1">
-                    <h2 className="fw-normal lh-1">WHAT IS A  <span
+                    <h2 className="fw-normal lh-1">DISCOVER THE BENEFITS OF A <span
                             className="text-body-secondary">BIOKINETICIST</span></h2>
-                    <p className="lead">A biokineticist is a clinical exercise specialist who: -</p>
-                    <p>
-                        <ul>
-                            <li>Primary function is to improve physical function & health care through exercise as a modality</li>
-                            <li>Improve a person's physical well-being and quality of life through individualised scientific assessment</li>
-                            <li>Orthopedic rehabilitation, injury prevention, chronic disease maintenance, performance enhancement (sport & work), and pre/post operative rehabilitation</li>
-                            <li>Evaluate & measure: 
-                                <ul>
-                                    <li>body posture</li>
-                                    <li>body composition</li>
-                                    <li>blood pressure</li>
-                                    <li>glucose level</li>
-                                    <li>lung function</li>
-                                    <li>heart rate</li>
-                                    <li>fitness</li>
-                                    <li>muscle strength</li>
-                                    <li>endurance</li>
-                                    <li>power</li>
-                                    <li>flexibility</li>
-                                    <li>and other health screenings</li>
-                                </ul></li>
-                            <li>Final phase rehabilitation</li>
-                        </ul>
-                    </p>
+                    
+                    A Biokineticist is an exercise specialist who focuses on promoting optimal health and well-being through exercise-based interventions. We aim to assess, treat, and prevent various medical conditions and injuries. 
+
+                    <h5 className="featurette-sub-heading">Injury rehabilitation:</h5>
+                    A Biokineticist can design an individualized training program to help regain strength, mobility, and function after you have suffered a sport-related injury or an orthopedic procedure. 
+
+                    <h5 className="featurette-sub-heading">Physical performance:</h5>
+                    A Biokineticist will help you reach peak performance. We provide expert guidance on sport strength and conditioning, endurance training, flexibility, and injury prevention for most to all sport types, individual or team sports do not matter if you are in primary school, high school, university, an adult, or a professional athlete. 
+
+                    <h5 className="featurette-sub-heading">Optimal health:</h5>
+                    We have a very important role in improving physical health. We promote physical health by thoroughly assessing the body composition while focusing on aspects such as BMI, Fat %, Muscle mass, circumference, and metabolism. A Biokineticist does a screening of blood pressure, glucose, and sugar levels. We do wellness coaching where we guide and advise the patient on how to have healthy lifestyle habits and promote overall wellbeing. 
+
+                    <h5 className="featurette-sub-heading">Chronic disease management:</h5>
+                    A Biokineticist can help manage and improve chronic disease symptoms, enhance the quality of life, and reduce the risk of complications. We are specialists in cardiovascular disease, respiratory disease, metabolic diseases such as obesity and diabetes, Inflammatory diseases such as arthritis, and neurological and mental conditions. 
+
+                    <h5 className="featurette-sub-heading">Special population treatments:</h5>
+                    A Biokineticist aims to improve acts of daily living, and physical functioning and enhance the quality of life of children, geriatric, disabled, impaired, pre and post-natal through exercise interventions and specialized knowledge and skills. 
+
+                    <h5 className="featurette-sub-heading">What to expect during your Biokineticist appointment</h5>
+                    During your first appointment, the Biokineticist will conduct a thorough physical assessment to determine individual needs and goals. We start by asking about your medical history, assess your posture, conduct physical testing, and discuss lifestyle habits and exercise preferences. Based on the information gathered we then develop your personalized exercise program that meets your needs. Regular follow-up sessions are booked to allow the Biokineticist to follow your progression, make necessary adjustments, and provide proper technique and form to prevent further injuries.
                 </div>
             </div>
         </div>
+
         <div className="container marketing">
             <hr className="featurette-divider" />
             <div className="row featurette">
@@ -53,7 +73,7 @@ const Featurette: React.FC = () => {
                     </p>
                 </div>
                 <div className="col-md-5 order-md-1">
-                    <img src="assets/jpg/what_is_biokineticist.jpg" alt="What is a biokineticist" className="featurette-image img-fluid mx-auto" />
+                    <StaticImage src="assets/jpg/what_is_biokineticist.jpg" alt="January Promo" className="featurette-image img-fluid mx-auto" />
                 </div>
             </div>
 
@@ -64,32 +84,16 @@ const Featurette: React.FC = () => {
                     <h2 className="fw-normal lh-1">AUGUST <span
                             className="text-body-secondary">WINTER FAT PROMOTION</span></h2>
                     <p className="lead">For Only R580.00</p>
-                    <p>
-                        <ul>
-                            <li>Initial evaluation and 1 re-evaluation on week 4 to ensure progress (BMI, Body fat %, Physical Fitness)</li>
-                            <li>Once off health screening (Blood pressure, glucose & cholesterol)</li>
-                            <li>Two +-45 minute exercise sessions per week under Biokineticist supervision</li>
-                            <li>3-4 x per week home exercise program</li>
-                            <li>Lifestyle & diet guidance</li>
-                        </ul>
-                    </p>
+                    <ul>
+                        <li>Initial evaluation and 1 re-evaluation on week 4 to ensure progress (BMI, Body fat %, Physical Fitness)</li>
+                        <li>Once off health screening (Blood pressure, glucose & cholesterol)</li>
+                        <li>Two +-45 minute exercise sessions per week under Biokineticist supervision</li>
+                        <li>3-4 x per week home exercise program</li>
+                        <li>Lifestyle & diet guidance</li>
+                    </ul>
                 </div>
                 <div className="col-md-5">
-                    <img src="assets/jpg/advert_500x500.jpg" alt="August Winter Promotion" className="featurette-image img-fluid mx-auto" />
-                </div>
-            </div>
-
-            <hr className="featurette-divider" />
-
-            <div className="row featurette">
-                <div className="col-md-7 order-md-2">
-                    <h2 className="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span
-                            className="text-body-secondary">See for yourself.</span></h2>
-                    <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of
-                        how this layout would work with some actual real-world content in place.</p>
-                </div>
-                <div className="col-md-5 order-md-1">
-                    <img src="assets/jpg/what_is_biokineticist.jpg" alt="What is a biokineticist" className="featurette-image img-fluid mx-auto" />
+                    <StaticImage src="assets/jpg/advert_500x500.jpg" alt="August Winter Promotion" className="featurette-image img-fluid mx-auto" />
                 </div>
             </div>
 
@@ -97,20 +101,33 @@ const Featurette: React.FC = () => {
 
             <div className="row featurette">
                 <div className="col-md-7">
-                    <h2 className="featurette-heading fw-normal lh-1">And lastly, this one. <span
-                            className="text-body-secondary">Checkmate.</span></h2>
-                    <p className="lead">And yes, this is the last block of representative placeholder content. Again, not
-                        really intended to be actually read, simply here to give you a better view of what this would
-                        look like with some actual content. Your content.</p>
+                    <h2 className="featurette-heading fw-normal lh-1">MONIQUE STRYDOM <span
+                            className="text-body-secondary">Qualifications</span></h2>
+                    <h5 className="featurette-sub-heading">Physical Health</h5>
+                    Wellness Coaching, Physical Health Promotion & Preventative Treatment. Full Body Assessment (Including BMI and Body Fat Percentage), Health screenings and lifestyle management
+
+                    <h5 className="featurette-sub-heading">Sport Performance</h5> 
+                    Sport Injury Rehabilitation, Sport Specific Training & Conditioning, Injury Prevention & Muscular Imbalance Rehabilitation. 
+
+                    <h5 className="featurette-sub-heading">Chronic Conditions</h5>
+                    Cardiac & Stroke Rehabilitation, Arthritis/Osteoporosis Rehabilitation, Obesity/ Weight Management, elevated glucose and cholesterol levels, Pulmonary disorders, Neurological & mental conditions etc. 
+
+                    <h5 className="featurette-sub-heading">Orthopaedics</h5>
+                    Clinical Assessment, Musculoskeletal Rehabilitation, Postural Deformities, Pre & Post Operative Final Phase Rehabilitation, Chronic pain condition
+
+                    <h5 className="featurette-sub-heading">Special population</h5>
+                    Children, Geriatric, disabled and impaired, pre and post natal, general population
+
+                    <h5 className="featurette-sub-heading">Other</h5>
+                    Pilates, Weight Loss Programs, Rebound
                 </div>
                 <div className="col-md-5">
-                    <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                        height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
-                        preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%"
-                            fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-                    </svg>
+                    <GatsbyImage image={featureImage}
+                            alt="What is a biokineticist" 
+                            className="featurette-image img-fluid mx-auto" />
+                    <GatsbyImage image={featureImage}
+                            alt="What is a biokineticist" 
+                            className="featurette-image img-fluid mx-auto" />
                 </div>
             </div>
 
