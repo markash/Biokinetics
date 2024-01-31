d1ad1e59e00e1fdd834233bcf9b87ff49df53434
Mark Ashworth <mp.ashworth@gmail.com>
2024-01-31 22:30:11 +0200

    Refine footer with address, logo and social info

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Header from "./header"
 import { Link, graphql, useStaticQuery } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,10 +13,19 @@ import {
   faInstagram,
 } from '@fortawesome/free-brands-svg-icons'
 
+import {
+  faPhone,
+  faEnvelope,
+  faLocationDot,
+} from '@fortawesome/free-solid-svg-icons'
+
 library.add(
   faFacebook,
   faXTwitter, 
   faInstagram,
+  faPhone,
+  faEnvelope,
+  faLocationDot
 )
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -36,11 +46,25 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <Header title={title} />
             <main>{children}</main>
             <footer>
-              <div className="row bg-body-tertiary social-banner">
+              <div className="row bg-footer-gray footer-row">
                 <div className="col-12">
-                    <Link to="https://www.facebook.com/MoniqueStrydomBiokineticist?mibextid=LQQJ4d" aria-label="Facebook"><FontAwesomeIcon icon={faFacebook} size="2x" className="social-icon" /></Link>
-                    <Link to="https://www.instagram.com/msbiokineticist_witbank?igsh=MXhxN3BudmM3bzhlOA%3D%3D&utm_source=qr" aria-label="Instagram"><FontAwesomeIcon icon={faInstagram} size="2x" className="social-icon"/></Link>
-                    <Link to="https://twitter.com/" aria-label="Twitter"><FontAwesomeIcon icon={faXTwitter} size="2x" className="social-icon"/></Link>
+                  <div className="row">
+                    <div className="col-6 col-md-3">
+                      <StaticImage src="../images/logo/logo-white.webp" alt="Monique Strydom Biokineticist / Biokinetikus 🥇🧘🏻‍♀️🏌🏻‍♀️" placeholder="blurred" width={377} height={50} />
+                      <p className="text-white social-banner">
+                      <Link to="https://www.facebook.com/MoniqueStrydomBiokineticist?mibextid=LQQJ4d" aria-label="Facebook"><FontAwesomeIcon icon={faFacebook} size="1x" className="social-icon" /></Link>
+                      <Link to="https://www.instagram.com/msbiokineticist_witbank?igsh=MXhxN3BudmM3bzhlOA%3D%3D&utm_source=qr" aria-label="Instagram"><FontAwesomeIcon icon={faInstagram} size="1x" className="social-icon"/></Link>
+                      <Link to="https://twitter.com/" aria-label="Twitter"><FontAwesomeIcon icon={faXTwitter} size="1x" className="social-icon"/></Link>
+                      </p>
+                    </div>
+                    <div className="col-6 col-md-3 text-white">
+                      <address>
+                          <FontAwesomeIcon icon={faLocationDot} size="1x" /> Elizabeth Avenue, Hoërskool Generaal Hertzog Stadium 1035 Witbank, South Africa<br />
+                          <FontAwesomeIcon icon={faPhone} size="1x" /> 076 633 6503<br />
+                          <FontAwesomeIcon icon={faEnvelope} size="1x" /> Bio.witbank@gmail.com<br />
+                      </address>
+                    </div>
+                  </div>
                 </div>
               </div>
             </footer>
